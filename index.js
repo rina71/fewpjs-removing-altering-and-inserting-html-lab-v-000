10756512e7b9c002b7a490f5f3aecd40b1ac6607
Yurina Shin <yurinas@gmail.com>
2020-06-13 04:01:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 main.remove();
-let element = document.createElement('div')
-document.body.appendChild(element)
-let h = document.createElement('h1');
-h.setAttribute('id', 'victory');
-document.body.appendChild(h)
-// let h = document.createElement('h1')
-// document.body.appendChild(h)
-let newHeader = document.getElementById("h1#victory");
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.className = 'victory'
 newHeader.innerHTML = "Make sure you create an <h1> with id 'victory' with a sweet message in it"
