b5c63fb9383e3c59de630009efdcc4dd1de77ebf
Yurina Shin <yurinas@gmail.com>
2020-06-13 04:03:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ main.remove();
 let newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.className = 'victory'
-newHeader.innerHTML = "Make sure you create an <h1> with id 'victory' with a sweet message in it"
+newHeader.innerHTML = "YOUR-NAME is the champion"
