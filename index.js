26e877826298d2902a8f87fc94853fe9327352da
Yurina Shin <yurinas@gmail.com>
2020-06-13 03:40:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 main.remove();
-// document.body.appendChild(div)
+document.body.appendChild("div")
 let newHeader = document.querySelector("h1#victory");
 newHeader.innerHTML = "Make sure you create an <h1> with id 'victory' with a sweet message in it"
