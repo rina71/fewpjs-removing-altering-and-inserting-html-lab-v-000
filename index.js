d79d63316e1667185cfeb7e3cbfd3cbbd41da4ea
Yurina Shin <yurinas@gmail.com>
2020-06-13 03:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 main.remove();
 let element = document.createElement('div')
 document.body.appendChild(element)
-document.createElement('h1')
+let h = document.createElement('h1');
+h.setAttribute('id', 'victory');
 // let h = document.createElement('h1')
 // document.body.appendChild(h)
 let newHeader = document.getElementById("h1#victory");
